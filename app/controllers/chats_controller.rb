567fb31a8269e7c5ee7b0d9366b7a7bf3fb4e6ae
Djamil Habes <habes.djamil@gmail.com>
2025-11-12 12:26:23 +0100

    Worked on the Chats controller and modified the routes of chats

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,2 +1,37 @@
 class ChatsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_chat, only: [:show, :destroy]
+  before_action :authorize_destroy, only: [:destroy]
+
+  def index
+    @chats = current_user.chats
+  end
+
+  def show
+  end
+
+  def destroy
+    if @chat.destroy
+      flash[:notice] = "Le chat a été supprimé avec succés"
+      redirect_to root_path, status: see_other
+    else
+      flash[:alert] = "Le chat n'a pas pu être supprimé!"
+      redirect_to @chat, status: unprocessable_entity
+    end
+    redirect_to root_path, status: see_other
+  end
+
+  private
+
+  def find_chat
+    @chat = Chat.find(params[:id])
+  end
+
+  def authorize_destroy
+    unless @chat.user == current_user
+      flash[:alert] = "Vous n'êtes pas autorisé à effectuer cette action"
+      redirect_to root_path
+      return
+    end
+  end
 end
