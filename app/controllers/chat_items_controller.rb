d1e6a3b03723b376f2eee1db0949ea49db327b7d
Djamil Habes <habes.djamil@gmail.com>
2025-11-12 17:32:51 +0100

    Final chatItems controller

--- app/controllers/chat_items_controller.rb
+++ app/controllers/chat_items_controller.rb
@@ -1,34 +1,32 @@
 class ChatItemsController < ApplicationController
   before_action :authenticate_user!
-
   def new
     @chat_item = ChatItem.new
     @items = current_user.items
   end
-
   def create
-    raise
-    @chat = Chat.new
-    @chat.user = current_user
-    unless @chat.save
-      flash[:alert] = "Impossible de démarrer le chat"
-    end
-
-    choosen_item_ids = params
-    @selected_items = Item.find(choosen_item_ids)
-
-    @selected_items.each do |original_item|
-      chat_item = @chat.chat_items.create!(
+    @chat = Chat.create!(user_id: current_user.id)
+    # unless @chat.save
+    #   flash[:alert] = "Impossible de démarrer le chat"
+    # end
+    @retrieved_items = params[:chat_item][:item_id] # ["", 1, 2, 3]
+    @cleaned_retrieved_items = @retrieved_items.reject(&:empty?) # Enlève le empty à l'index 0 -> [1, 2, 3]
+    @selected_items = @cleaned_retrieved_items.map { |selected_item| Item.where(id: selected_item).uniq } # On comparait les chiffres dans les array avec les ID des Items
+    # .flatten
+    # On passe de:
+    # [[Instance], [Instance], [Instance]]
+    # à
+    # [Instance, Instance, Instance]
+    @selected_items.flatten.each do |original_item|
+      @chat.chat_items.create!(
         item_id: original_item.id,
         name: original_item.name,
         brand: original_item.brand,
-        indice_gly: original_item.indice_gly,
-        ratio_glucide: original_item.ratio_glucide,
-        category: original_item.category,
       )
     end
-
     redirect_to chat_path
   end
-
 end
+        # indice_gly: original_item.indice_gly,
+        # ratio_glucide: original_item.ratio_glucide,
+        # category: original_item.category
