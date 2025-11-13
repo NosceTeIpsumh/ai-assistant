2f4366b7103b10eb4850604f673ac9ca51b097ee
martin dussenneMartin Dussenne <martin.dussenne@gmail.com>
2025-11-13 16:11:46 +0100

    Installation gems kramdown, rouge & kramdown-parser-gfm

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def markdown(text)
+    return "" if text.blank?
+
+    Kramdown::Document.new(
+      text,
+      input: "GFM",
+      syntax_highlighter: "rouge",
+      hard_wrap: true
+    ).to_html.html_safe
+  end
 end
